e6a7f0daf11d6969bcef61938e326e1d4e648ce2
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2021-04-01 13:28:18 +0500

    update script.js
    set a timer with the function and typing qurry

--- script.js
+++ script.js
@@ -27,35 +27,35 @@ quoteInputElement.addEventListener('input', () => {
   if (correct) renderNewQuote()
 })
 
-// function getRandomQuote() {
-//   return fetch(RANDOM_QUOTE_API_URL)
-//     .then(response => response.json())
-//     .then(data => data.content)
-// }
+function getRandomQuote() {
+  return fetch(RANDOM_QUOTE_API_URL)
+    .then(response => response.json())
+    .then(data => data.content)
+}
 
-// async function renderNewQuote() {
-//   const quote = await getRandomQuote()
-//   quoteDisplayElement.innerHTML = ''
-//   quote.split('').forEach(character => {
-//     const characterSpan = document.createElement('span')
-//     characterSpan.innerText = character
-//     quoteDisplayElement.appendChild(characterSpan)
-//   })
-//   quoteInputElement.value = null
-//   startTimer()
-// }
+async function renderNewQuote() {
+  const quote = await getRandomQuote()
+  quoteDisplayElement.innerHTML = ''
+  quote.split('').forEach(character => {
+    const characterSpan = document.createElement('span')
+    characterSpan.innerText = character
+    quoteDisplayElement.appendChild(characterSpan)
+  })
+  quoteInputElement.value = null
+  startTimer()
+}
 
-// let startTime
-// function startTimer() {
-//   timerElement.innerText = 0
-//   startTime = new Date()
-//   setInterval(() => {
-//     timer.innerText = getTimerTime()
-//   }, 1000)
-// }
+let startTime
+function startTimer() {
+  timerElement.innerText = 0
+  startTime = new Date()
+  setInterval(() => {
+    timer.innerText = getTimerTime()
+  }, 1000)
+}
 
-// function getTimerTime() {
-//   return Math.floor((new Date() - startTime) / 1000)
-// }
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000)
+}
 
-// renderNewQuote()
+renderNewQuote()
