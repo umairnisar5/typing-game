ed1490aa9a695f8a078fd8b5b95e4905b7f5c561
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2021-04-01 13:00:01 +0500

    update script.js
    create  a variable and declare in to function

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-// const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
-// const quoteDisplayElement = document.getElementById('quoteDisplay')
-// const quoteInputElement = document.getElementById('quoteInput')
-// const timerElement = document.getElementById('timer')
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
 
-// quoteInputElement.addEventListener('input', () => {
-//   const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-//   const arrayValue = quoteInputElement.value.split('')
+quoteInputElement.addEventListener('input', () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+  const arrayValue = quoteInputElement.value.split('')
 
-//   let correct = true
-//   arrayQuote.forEach((characterSpan, index) => {
-//     const character = arrayValue[index]
-//     if (character == null) {
-//       characterSpan.classList.remove('correct')
-//       characterSpan.classList.remove('incorrect')
-//       correct = false
-//     } else if (character === characterSpan.innerText) {
-//       characterSpan.classList.add('correct')
-//       characterSpan.classList.remove('incorrect')
-//     } else {
-//       characterSpan.classList.remove('correct')
-//       characterSpan.classList.add('incorrect')
-//       correct = false
-//     }
-//   })
+  let correct = true
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index]
+    if (character == null) {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.remove('incorrect')
+      correct = false
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect')
+    } else {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.add('incorrect')
+      correct = false
+    }
+  })
 
-//   if (correct) renderNewQuote()
-// })
+  if (correct) renderNewQuote()
+})
 
 // function getRandomQuote() {
 //   return fetch(RANDOM_QUOTE_API_URL)
